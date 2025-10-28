aaa48e26164512205feb9fd97263e2785d5477e6
Muhammadjasim1122 <m.jasimkhan250@gmail.com>
2025-10-28 20:25:27 +0500

    build running

--- src/app/main_sidebar/shift-attendance/page.js
+++ src/app/main_sidebar/shift-attendance/page.js
@@ -2,7 +2,12 @@
 
 import React from 'react';
 import AttendanceDashboard from '@/components/main/hr/attendance-dashboard';
+import { DashboardProvider } from '@/components/contexts/DashboardContext';
 
 export default function ShiftAttendancePage() {
-  return <AttendanceDashboard />;
+  return (
+    <DashboardProvider>
+      <AttendanceDashboard />
+    </DashboardProvider>
+  );
 }
--- src/app/shift-attendance/page.js
+++ src/app/shift-attendance/page.js
@@ -2,9 +2,12 @@
 
 import React from 'react';
 import AttendanceDashboard from '@/components/main/hr/attendance-dashboard';
+import { DashboardProvider } from '@/components/contexts/DashboardContext';
 
 export default function ShiftAttendancePage() {
   return (
-    <AttendanceDashboard />
+    <DashboardProvider>
+      <AttendanceDashboard />
+    </DashboardProvider>
   );
 }
